c4894703d7ce4146ffb8c1290c5273516e8815c2
sudyakov <sudyakov_git@icloud.com>
2024-01-20 13:39:35 +0200

    ~

--- crates/test-gpu/src/main.rs
+++ crates/test-gpu/src/main.rs
@@ -1,9 +1,28 @@
 //
 use rust_gpu_tools::{cuda, opencl, program_closures, Device, GPUError, Program, Vendor};
 
-
-fn main (){
-
-println!("Running main tests...");
-
+// Вывести на екран список доступных устройств с их описанием.
+fn show_list_devices(devices: &Vec<&Device>) {
+    println!("Available devices:");
+    for device in devices {
+        println!("- {} ({})", device.name(), device.vendor());
+        println!("    Memory: {} MB", device.memory() / 1024 / 1024);
+        println!("    Compute units: {}", device.compute_units());
+        println!("    Compute capability: {:?}", device.compute_capability());
+    }
 }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let devices = Device::all();
+    show_list_devices(&devices);
+
+    // Найти первое CUDA устройство в списке devices
+    let first_cude_device = devices.iter().find(|d| d.vendor() == Vendor::Nvidia); 
+    // вывод имени первого устройства
+    if let Some(device) = first_cude_device {
+    println!("First CUDA device: {}", device.name());
+}
+    Ok(())
+
+}// end main function
