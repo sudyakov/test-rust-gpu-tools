985ac6fb4a1080f094519315c2010c4b09309e4a
sudyakov <sudyakov_git@icloud.com>
2024-01-21 07:09:47 +0200

    ~

--- crates/test-gpu/src/main.rs
+++ crates/test-gpu/src/main.rs
@@ -13,14 +13,32 @@ fn show_list_devices(devices: &Vec<&Device>) {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Получить список доступных устройств
     let devices = Device::all();
     show_list_devices(&devices);
 
     // Найти первое CUDA устройство в списке devices
-    let first_cude_device = devices.iter().find(|d| d.vendor() == Vendor::Nvidia);
+    let first_cude_device = devices
+        .iter()
+        .find(|d| d.vendor() == Vendor::Nvidia)
+        .unwrap();
     // Вывод имени первого устройства
-    if let Some(device) = first_cude_device {
-        println!("First CUDA device: {}", device.name());
-    }
+    println!("First CUDA device: {}", first_cude_device.name());
+
+    // Define test data
+    let test_data: Vec<u32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+    // Вывест на экран занчение тестовых данных
+    println!("Test data: {:?}", test_data);
+
+
+
+    //println!("Result: {:?}", result);
+
     Ok(())
-} // end main function
+}
+
+
+
+
+
