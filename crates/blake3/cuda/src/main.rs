f3ea6e7d4865e31e6e9a2a3139c17d3316e8a851
Sudyakov <sudyakov_git@icloud.com>
2024-02-06 15:51:27 +0200

    ~

--- crates/blake3/cuda/src/main.rs
+++ crates/blake3/cuda/src/main.rs
@@ -3,12 +3,12 @@ use std::time::Instant;
 use std::vec::*;
 
 pub fn main() {
-    let repeat_count = 1000;
+    let repeat_count = 10000;
 
     // NVIDIA GeForce RTX 4080 cores=9728, tmus=304, rops=112, memory=16Gb
     // total number of threads is global_work_size * local_work_size 
-    let global_work_size = 32;
-    let local_work_size = 1024;
+    let global_work_size = 1;
+    let local_work_size = 1;
 
     let all_devices = Device::all();
     print_all_devices(&all_devices);
@@ -18,8 +18,7 @@ pub fn main() {
 
     let test_data: Vec<u32> = vec![
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-        26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-        49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+        26, 27, 28, 29, 30, 31, 32,
     ];
     println!("test_data:\n{:?}\n", test_data);
 
@@ -54,11 +53,9 @@ pub fn main() {
 
     // Замеряем время после выполнения функции
     let duration = start.elapsed();
+    println!("CUDA result: \n{:?}", cuda_result.unwrap());
     // Выводим замеренное время
     println!("Time elapsed in expensive_function() is: {:?}", duration);
-
-    println!("CUDA result: \n{:?}", cuda_result.unwrap());
-
     println!("CUDA test passed\n");
 
     // Сортируем на процессоре
